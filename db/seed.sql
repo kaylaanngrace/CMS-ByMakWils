f6a34d7b88c5e972a0a73410157e907b74a74e07
MakWils <kaylaanngrace@gmail.comgit remote add origin git@github.com:kaylaanngrace/horiseon-optimize-accessibility.git>
2022-03-18 22:06:01 -0500

    refactored seed code

--- db/seed.sql
+++ db/seed.sql
@@ -2,41 +2,34 @@
 USE CMS_db;
 
 /* data for deparment table */
-INSERT INTO department (name)
-VALUES ("Information Tech");
-INSERT INTO department (name)
-VALUES ("Graphic Design");
-INSERT INTO department (name)
-VALUES ("Web Programming");
-INSERT INTO department (name)
-VALUES ("Human Resources");
+INSERT INTO department 
+    (name)
+VALUES 
+    ("Information Tech"),
+    ("Graphic Design"),
+    ("Web Programming"),
+    ("Human Resources")
 
 /* data for jobRole table */
-INSERT INTO jobRole (title, salary, department_id)
-VALUES ("IT Lead", 100000, 1);
-INSERT INTO jobRole (title, salary, department_id)
-VALUES ("FullStack Developer", 150000, 3);
-INSERT INTO jobRole (title, salary, department_id)
-VALUES ("Graphic Designer", 120000, 2);
-INSERT INTO jobRole (title, salary, department_id)
-VALUES ("Human Resources Lead", 125000, 4);
-INSERT INTO jobRole (title, salary, department_id)
-VALUES ("BackEnd Developer", 125000, 3);
+INSERT INTO jobRole 
+    (title, salary, department_id)
+VALUES 
+    ("IT Lead", 100000, 1),
+    ("FullStack Developer", 150000, 3),
+    ("Graphic Designer", 120000, 2),
+    ("Human Resources Lead", 125000, 4),
+    ("BackEnd Developer", 125000, 3)
+
 
 /* data for employee table */
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jane", "Doe", 1, null);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Doe", 2, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Smith", 3, null);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Megan", "Bettis", 4, 3);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Leon", "Jones", 5, 3);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Sara", "Lee", 2, 5);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Betty", "White", 4, null);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Fredick", "Hooper", 1, 2);
+INSERT INTO employee 
+    (first_name, last_name, role_id, manager_id)
+VALUES 
+    ("Jane", "Doe", 1, null),
+    ("John", "Smith", 3, null),
+    ("John", "Doe", 2, 1),
+    ("Megan", "Bettis", 4, 3),
+    ("Leon", "Jones", 5, 3),
+    ("Sara", "Lee", 2, 5),
+    ("Betty", "White", 2, 5),
+    ("Fredick", "Hooper", 3, 2)
